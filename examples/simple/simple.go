10e69cdf651afa6ff89698daa45a7336ff8cd7bf
e-zk <e-zk@users.noreply.github.com>
2021-07-05 16:15:14 +1000

    Update example

---
+++ examples/simple/simple.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/e-zk/subc"
+)
+
+func main() {
+	var (
+		force     bool
+		name      string
+		byteLimit int
+	)
+
+	subc.Sub("add").StringVar(&name, "n", "defaultname", "name of file to create")
+	subc.Sub("add").IntVar(&byteLimit, "b", 128, "number of random bytes to write to file")
+	subc.Sub("add").BoolVar(&force, "f", false, "force/overwrite file")
+
+	subc.Sub("remove").StringVar(&name, "n", "defaultname", "name of file to remove")
+	subc.Sub("remove").BoolVar(&force, "f", false, "force remove file")
+
+	err := subc.Parse()
+	if err == subc.ErrSubcNotExist {
+		// show usage if an unknown subcommand is given
+		print("Unknown subcommand\n")
+		subc.Usage()
+	} else if err != nil {
+		panic(err)
+	}
+}
