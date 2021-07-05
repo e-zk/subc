8e99a7a2544ba10c54c2453857fb94988d8f02a5
e-zk <e-zk@users.noreply.github.com>
2021-07-05 15:58:51 +1000

    Improve Usage function, add ParseArgs function
    * Add Output()/SetOutput() for getting/setting the output writer
    * Add ParseArgs(args) for manually specifying arguments (as opposed to using os.Args)

--- subc.go
+++ subc.go
@@ -3,40 +3,70 @@ package subc
 import (
 	"errors"
 	"flag"
+	"fmt"
+	"io"
 	"os"
 )
 
-// errors
+// ErrSubcNotExist is returned when a subcommand that does not exist is requested.
+var ErrSubcNotExist = errors.New("Subcommand does not exist")
+
 var (
-	ErrSubcNotExist = errors.New("Subcommand does not exist")
+	subcommands            = make(map[string]*flag.FlagSet)
+	outputWriter io.Writer = os.Stderr
 )
 
-var subcommands = make(map[string]*flag.FlagSet)
+// Prints a usage message documenting all defined subcommands and their flags.
+var Usage = func() {
+	fmt.Fprintf(outputWriter, "Usage of %s [", os.Args[0])
+	for name, _ := range subcommands {
+		fmt.Fprintf(outputWriter, "%s|", name)
+	}
+	fmt.Fprintf(outputWriter, "]\n")
 
-// add new/access existing subcommand
+	for name, f := range subcommands {
+		fmt.Fprintf(outputWriter, "%s:\n", name)
+		f.PrintDefaults()
+	}
+}
+
+// Output returns the destination writer for usage and error messages.
+func Output() io.Writer {
+	return outputWriter
+}
+
+// Sets destination for all subcommand usage and messages. By default this is os.Stderr.
+func SetOutput(output io.Writer) {
+	outputWriter = output
+}
+
+// Sub adds a new subcommand or accesses and existing subcommand.
 func Sub(name string) (s *flag.FlagSet) {
 	_, ok := subcommands[name]
 	if !ok {
 		// create the flagset if it doesn't exist
 		subcommands[name] = flag.NewFlagSet(name, flag.ExitOnError)
+		subcommands[name].SetOutput(outputWriter)
 	}
 	return subcommands[name]
 }
 
-// concat all flag usages
-func Usage() string {
-	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
-	for f, n := range subcommands {
-		fmt.Fprintf("\t%s\n", f.PrintDefaults())
-	}
-}
-
-// parse the subcommand requested
-// TODO: throw up errors from flag
+// Parse the subcommand input (os.Args[1]) and arguments.
 func Parse() error {
 	c, ok := subcommands[os.Args[1]]
 	if !ok {
 		return ErrSubcNotExist
 	}
+
 	return c.Parse(os.Args[2:])
+}
+
+// Parse the given subcommand input and arguments.
+func ParseArgs(args []string) error {
+	c, ok := subcommands[args[1]]
+	if !ok {
+		return ErrSubcNotExist
+	}
+
+	return c.Parse(args[2:])
 }
